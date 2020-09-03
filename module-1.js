a1d2caeafba74811cbc8f9665c321e8e44d8dfcb
YehorSavin <yagaro4ek@gmail.com>
2020-09-03 18:34:55 +0300

    module 1 part 1

---
+++ module-1.js
@@ -0,0 +1,54 @@
+// const name = 'Yehor';
+// let age = +prompt ('введите ваш возраст');
+// const message = 'hello world' + name;
+// console.log(message);
+
+// let newMessage = `My name is ${age}`;
+// console.log(newMessage);
+// age = 20;
+// console.log(newMessage);
+// console.log(age);
+// age = +age;
+// answer = confirm (`хуй соси`);
+// console.log(answer);
+// console.log(typeof answer);
+
+// let newQuestion = 'вы любите js?';
+// let newAnswer = confirm (newQuestion);
+// console.log(typeof newAnswer);
+
+let num = 10;
+let boolNum = Boolean(num);
+console.log(boolNum);
+
+num = 0;
+boolNum = Boolean(num);
+console.log(boolNum);
+
+let str = 'Gena';
+let boolStr = Boolean(str);
+console.log(boolStr);
+
+str = ``;
+boolStr = Boolean(str);
+console.log(boolStr);
+
+str = ` `;
+boolStr = Boolean(str);
+console.log(boolStr);
+
+let isOpen;
+console.log(isOpen);
+console.log(typeof isOpen);
+
+let boolUnd = Boolean(isOpen);
+console.log(boolUnd);
+console.log(typeof boolUnd);
+
+isOpen = null;
+console.log(isOpen);
+console.log(typeof isOpen);
+
+let boolNull = Boolean (isOpen);
+console.log(boolNull)
+console.log(typeof boolNull)
