efb6e17b7351fead899879ce8f6ace5b5175cb8d
YehorSavin <yagaro4ek@gmail.com>
2020-09-07 17:35:45 +0300

    progress

--- module-1.js
+++ module-1.js
@@ -52,3 +52,91 @@ console.log(typeof isOpen);
 let boolNull = Boolean (isOpen);
 console.log(boolNull)
 console.log(typeof boolNull)
+
+
+// -----------------------------------------------------------------------------
+
+
+// const showMeArguments = functions(...args) {
+
+// }
+
+// ----------------------Task1
+
+// const logItems = function(array) {
+//   for(let i =0; i < array.length; i++) {
+//     console.log(`${i+1}-${array[i]}`)
+//   }
+// };
+
+
+// logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+
+// logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+
+// logItems([100,`CSS`,`JS`,[`react`, `css`]]);
+
+// -----------------------Task2
+
+// const a = function(message, pricePerWord) {
+//   let b = message.split(` `);
+//   // console.log(b);
+//   let c = b.length;
+//   // console.log(c);
+//   let d = pricePerWord * c;
+//   // console.log(d);
+//   return d;
+// };
+
+// console.log(
+//   a(
+//     'Proin sociis natoque et magnis parturient montes mus',
+//     10,
+//   ),
+// ); // 80
+
+// console.log(
+//   a(
+//     'Proin sociis natoque et magnis parturient montes mus',
+//     20,
+//   ),
+// ); // 160
+
+// console.log(
+//   a('Donec orci lectus aliquam est magnis', 40),
+// ); // 240
+
+// console.log(
+//   a('Donec orci lectus aliquam est magnis', 20),
+// ); // 120
+
+// ----------------------Task3
+
+const findLongestWord = function(string) {
+  let arrayWord = string.split(` `);
+  // console.log(arrayWord);
+  let longestWord = arrayWord[0];
+  let wordLength = arrayWord[0].length;
+  for(let i = 1; i < arrayWord.length; i++){
+    // console.log(arrayWord[i]);
+    // console.log(arrayWord[i].length);
+    if(wordLength < arrayWord[i].length) {
+      wordLength = arrayWord[i].length
+      // console.log(wordLength);
+      longestWord = arrayWord[i];
+      // console.log(longestWord);
+    }
+  }
+  return longestWord
+  // for (let word of arrayWord) {
+  //   console.log(typeof word);
+    
+  // }
+};
+
+
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+
+console.log(findLongestWord('Google do a roll'));
+
+console.log(findLongestWord('May the force be with you'));
